e28dd562bb7fb97221cd20488d16cfdc4df02091
zekesiegman <eas14751@uga.edu>
2022-04-18 21:46:24 -0400

    added models, not registered yet

--- mysite/home/admin.py
+++ mysite/home/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Profile
+from .models import Item
+from .models import Cart
 
 # Register your models here.
+
+admin.site.register(Profile)
+admin.site.register(Item)
+admin.site.register(Cart)
--- mysite/home/models.py
+++ mysite/home/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
+import django.utils.timezone
 
 # Create your models here.
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True, default=1)
+    cardNumber = models.CharField(max_length=500, default='')
+    expiration = models.DateField(default=django.utils.timezone.now)
+    billingAddress = models.CharField(max_length=250, default='')
+
+
+class Item(models.Model):
+    itemId = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=250)
+    price = models.IntegerField()
+    storage = models.IntegerField()
+    ram = models.IntegerField()
+    camera_res = models.IntegerField()
+    size = models.CharField(max_length=250)
+
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
+    item = models.ForeignKey('Item', on_delete=models.CASCADE, default=1)
